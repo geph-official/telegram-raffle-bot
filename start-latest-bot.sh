4ecf742c00b1a590dd5b2c2b65b1654c729e7d48
thisbefruit <thisbefruit@protonmail.com>
2023-09-08 14:48:43 -0400

    new

--- start-latest-bot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:/root/.cargo/bin
-
-bot_path='/root/telegram-raffle-bot/'
-config_path='/root/telegram-raffle-bot/config.yaml'
-
-cd $bot_path
-git pull
-RUST_LOG=telegram-raffle-bot cargo run --release -- -c $config_path
